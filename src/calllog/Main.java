38bc28fb27d8640e4c3be735c3d262bead4e29a9
Jean-Philippe Daigle <jpdaigle@gmail.com>
2012-11-27 19:10:45 -0500

    Cleaned up Main test program a bit

--- src/calllog/Main.java
+++ src/calllog/Main.java
@@ -2,19 +2,16 @@ package calllog;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) throws Exception {
-		LogParser parser = new LogParser("testlog.txt");
+		final String filename = "testlog.txt";
+		LogParser parser = new LogParser(filename);
 		CallLog log = parser.parse();
-		System.out.printf("Log loaded with %s entries\n", log.size());
+		System.out.printf("Log %s loaded with %s entries\n", filename, log.size());
 
-		int minutes = 24 * 60;
-
-		for (int i = 0; i < minutes; i++) {
+		int minutes = 18 * 60; // look at 18 hours
+		for (int i = 0; i < minutes; i += 30) {
 			String time = String.format("%02d:%02d", (int) (i / 60), i % 60);
-			System.out.printf("%s  ==> %s\n", time, Operations.getConcurrentCountAtTime(log, time));
+			System.out.printf("%s  ==> %s concurrent calls\n", time, Operations.getConcurrentCountAtTime(log, time));
 		}
 
 	}
